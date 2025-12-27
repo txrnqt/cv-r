71c7b2b3e552d2f8d0ce725f0b62f5f57e2b2d31
Jace Rodgers <jaransas2008@gmail.com>
2025-12-26 19:56:10 -0600

    model loads

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod app_state;
 mod camera;
 mod detections;
-
 use anyhow;
+use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut app = app_state::AppState::new("src/content/jetson_orinnano.onnx").await?;
+    println!("starting");
+    let model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("content/jetson_orinnano.onnx");
+    println!("model path {:?}", model_path);
+    let mut app = app_state::AppState::new(model_path).await?;
+    println!("model loaded");
     app.activate_camera("/dev/video0").await?;
 
     loop {
